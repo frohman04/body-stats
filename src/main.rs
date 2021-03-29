ecbbf8f7957f4dd690c685ba17fa0a8a70e463ab
Chris Lieb <chris.lieb@gmail.com>
2021-03-29 19:45:57 -0400

    Fix for breaking change in simplelog

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ mod timed;
 
 use calamine::{open_workbook, DeError, RangeDeserializerBuilder, Reader, Xlsx, XlsxError};
 use clap::{App, Arg};
-use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
+use simplelog::{ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
 use tempfile::NamedTempFile;
 use time::{Date, Duration};
 
@@ -29,6 +29,7 @@ fn main() {
         LevelFilter::Info,
         Config::default(),
         TerminalMode::Stderr,
+        ColorChoice::Auto,
     )])
     .unwrap();
 
